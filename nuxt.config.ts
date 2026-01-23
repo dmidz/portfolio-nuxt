84a8b616bb79a26bc5294a8306406c81f3a70dc1
David MARTIN <david@dmidz.com>
2026-01-23 11:39:02 +0100

    increase font-size + fix missing css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  // compatibilityDate: '2026-01-17',
+  experimental: { appManifest: false },
+  compatibilityDate: '2026-01-23',
   runtimeConfig: {
     mailerAuthUser: '',
     mailerAuthPass: '',
